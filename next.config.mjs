eb6e895e892c10e71ecf711bebc914e6ad0a678b
alizaidi123 <159936656+alizaidi123@users.noreply.github.com>
2025-01-21 23:29:37 +0500

    next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    domains: ['cdn.sanity.io'], // Replace with your actual image domain(s)
+  },
+};
 
 export default nextConfig;
